25d282dd24f97d1e16389938198bf70ac79a0858
Ryan Hart <ryachart@gmail.com>
2013-05-23 00:06:25 -0700

    Saving the locking choice and allowing locking

--- HowMuchMitigation.lua
+++ HowMuchMitigation.lua
@@ -10,8 +10,14 @@ function HowMuchMitigation_OnLoad(self)
 	self:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED");
 	self:RegisterEvent("PLAYER_ENTERING_WORLD");
 	self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+	self:RegisterEvent("PLAYER_REGEN_DISABLED");
+	self:RegisterEvent("PLAYER_REGEN_ENABLED");
 	self:SetScript("OnEvent",function(self, event, ...)HowMuchMitigation[event](self, ...)end);
 	HowMuchMitigation_MainText:SetText("0");
+	if ( HowMuchMitigation_Settings == nil ) then
+		HowMuchMitigation_Settings = { };
+		HowMuchMitigation_Settings.IsLocked = false;
+	end
 end
 
 function HowMuchMitigation:COMBAT_LOG_EVENT_UNFILTERED(timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destFlags2, ...)
@@ -24,21 +30,34 @@ function HowMuchMitigation:COMBAT_LOG_EVENT_UNFILTERED(timestamp, event, hideCas
 	end
 end
 
+function HowMuchMitigation:PLAYER_REGEN_DISABLED()
+	HowMuchMitigation:Check_Visibility();
+end
+
+function HowMuchMitigation:PLAYER_REGEN_ENABLED()
+	HowMuchMitigation:Check_Visibility();
+end
+
 function HowMuchMitigation:ACTIVE_TALENT_GROUP_CHANGED()
 	HowMuchMitigation:Check_Visibility();
 end
 
 function HowMuchMitigation:PLAYER_ENTERING_WORLD()
 	HowMuchMitigation:Check_Visibility();
-	self:RegisterForDrag("LeftButton");
 	
+	if (HowMuchMitigation_Settings.IsLocked) then
+		self:RegisterForDrag();
+	else
+		self:RegisterForDrag("LeftButton");
+	end
 	local appropriateSpellTexture = GetSpellTexture(HowMuchMitigation:Appropriate_Spell());
-	
 	HowMuchMitigation_MainButton1:SetNormalTexture(appropriateSpellTexture);
 end
 
 function HowMuchMitigation:Check_Visibility()
-	if ( HowMuchMitigation:Appropriate_Spell() ) then
+	local inCombat = UnitAffectingCombat("player");
+	local unlocked = not HowMuchMitigation_Settings.IsLocked;
+	if ( HowMuchMitigation:Appropriate_Spell() and (inCombat or unlocked)) then
 		HowMuchMitigation_Main:Show();
 	else
 		HowMuchMitigation_Main:Hide();
@@ -59,6 +78,18 @@ end
 function HowMuchMitigation:SlashCmd(message, editBox)
 	local command, rest = message:match("^(%S*)%s*(.-)$");
 	-- Nothing right now =/
+	if (command == "lock") then
+		if ( HowMuchMitigation_Settings.IsLocked ) then
+			HowMuchMitigation_Main:RegisterForDrag("LeftButton");
+			print("HMM: Frame unlocked.");
+			HowMuchMitigation_Settings.IsLocked = false;
+		else
+			HowMuchMitigation_Main:RegisterForDrag();
+			print("HMM: Frame locked.");
+			HowMuchMitigation_Settings.IsLocked = true;
+		end
+		HowMuchMitigation:Check_Visibility();
+	end
 end
 
 function HowMuchMitigation:Shield_Barrier_Value()
